c5f7b5c3735ccc9f3bfcef63bfbf20279d956019
daniel petcov <daniel.petcov@dotteam.md>
2024-12-25 00:42:04 +0200

    updated the images logic

--- src/data/components.ts
+++ src/data/components.ts
@@ -1,8 +1,10 @@
+import beQuiet from "../assets/components/be-quiet-pure-power-12-m-psu-product.avif"
+
 const components = [
     {
         name: "be quiet! PURE POWER 12 M 850W",
         price: 2722,
-        image: "/src/assets/components/be-quiet-pure-power-12-m-psu-product.avif",
+        image: beQuiet,
         link: "https://electro.md/psu-be-quiet-pure-power-12-m-850w?search=PURE%20POWER%2012%20M"
     }
 ]
