65e132ce4ed38327b381ac8df1ac57683196d369
daniel petcov <danielpetcov.work@gmail.com>
2024-12-25 01:40:22 +0200

    added more products

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -9,16 +9,16 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
     return (
-        <div className="flex flex-col items-center justify-between gap-6 bg-quaternary p-4 rounded-lg w-fit">
+        <div className="flex flex-col justify-between gap-6 bg-quaternary p-4 rounded-lg w-fit">
             <div className="w-full max-w-[300px] aspect-square">
-                <img src={product.image} alt={product.name} className="w-full h-full object-cover" />
+                <img src={product.image} alt={product.name} className="w-full h-full object-contain" />
             </div>
             <div className="flex flex-col  justify-between gap-6">
                 <h3 className="text-lg font-bold">{product.name}</h3>
                 <div className="flex justify-between gap-2 items-end">
                     <div>
                         <span className="text-mds font-semibold">Pret: </span>
-                        <span className="text-md font-semibold">{product.price}</span>
+                        <span className="text-md font-semibold">{product.price} mdl</span>
                     </div>
                     <a href={product.link} target="_blank" className="bg-secondary text-white px-4 py-2 rounded-md hover:bg-tertiary transition-all duration-300">Link produs</a>
                 </div>
--- src/data/components.ts
+++ src/data/components.ts
@@ -1,11 +1,24 @@
 import beQuiet from "../assets/components/be-quiet-pure-power-12-m-psu-product.avif"
-
+import ryzen7 from '../assets/components/procesor.webp'
+import hdd from '../assets/components/hdd.png'
 const components = [
     {
         name: "be quiet! PURE POWER 12 M 850W",
         price: 2722,
         image: beQuiet,
         link: "https://electro.md/psu-be-quiet-pure-power-12-m-850w?search=PURE%20POWER%2012%20M"
+    },
+    {
+        name: "Ryzen 7 5700X3D",
+        price: 3399,
+        image: ryzen7,
+        link: "https://999.md/ro/88293752"
+    },
+    {
+        name: "HDD SEAGATE BarraCuda 1TB",
+        price: 736,
+        image: hdd,
+        link: "https://www.fantastic.md/hdd-seagate-barracuda-compute-st1000dm010/139-217831"
     }
 ]
 
