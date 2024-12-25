ddf575050549cadc6376734b1b3a56aa3fd6c36d
daniel petcov <danielpetcov.work@gmail.com>
2024-12-25 10:57:40 +0200

    added a calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import MainPage from './pages/mainPage';
+import Calculator from './components/calculator';
 
 function App() {
   return (
     <>
       <div className="flex flex-col bg-primary min-h-screen py-10 gap-10 px-4">
         <h1 className="text-center text-2xl font-bold">Componente pc</h1>
+        <Calculator />
         <MainPage />
       </div>
     </>
---
+++ src/components/calculator.tsx
@@ -0,0 +1,22 @@
+import components from "../data/components";
+
+interface Component {
+    name: string;
+    price: number; 
+    link: string;
+    image: string;
+}
+
+const Calculator = () => {
+
+    const totalPrice = components.reduce((acc, component) => acc + component.price, 0);
+
+  return (
+    <div>
+        <h1 className="text-center text-2xl font-bold">Pret total: {totalPrice} mdl</h1>
+    </div>
+    
+  )
+}
+
+export default Calculator;
