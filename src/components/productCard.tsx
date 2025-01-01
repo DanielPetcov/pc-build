c5137b03e8bdb702bc08826c08c5bb79ba5d5918
daniel petcov <danielpetcov.work@gmail.com>
2025-01-01 14:54:23 +0200

    updated the responsive of the products and added new ones

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -9,18 +9,18 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
     return (
-        <div className="flex flex-col justify-between gap-6 bg-quaternary p-4 rounded-lg w-fit">
-            <div className="w-full max-w-[300px] aspect-square">
+        <div className="flex flex-col lg:justify-between gap-6 bg-quaternary p-4 rounded-lg w-full">
+            <div className="w-full max-w-[300px] aspect-square mx-auto h-[150px] lg:h-auto">
                 <img src={product.image} alt={product.name} className="w-full h-full object-contain" />
             </div>
-            <div className="flex flex-col  justify-between gap-6">
-                <h3 className="text-lg font-bold">{product.name}</h3>
-                <div className="flex justify-between gap-2 items-end">
+            <div className="flex flex-col  justify-between gap-6 h-full">
+                <h3 className="text-lg font-bold line-clamp-3">{product.name}</h3>
+                <div className="flex flex-col lg:flex-row justify-between gap-2 lg:items-end">
                     <div>
                         <span className="text-mds font-semibold">Pret: </span>
                         <span className="text-md font-semibold">{product.price} mdl</span>
                     </div>
-                    <a href={product.link} target="_blank" className="bg-secondary text-white px-4 py-2 rounded-md hover:bg-tertiary transition-all duration-300">Link produs</a>
+                    <a href={product.link} target="_blank" className="bg-secondary text-white text-center px-4 py-2 rounded-md hover:bg-tertiary transition-all duration-300">Link produs</a>
                 </div>
             </div>
         </div>
--- src/data/components.ts
+++ src/data/components.ts
@@ -1,6 +1,8 @@
 import beQuiet from "../assets/components/be-quiet-pure-power-12-m-psu-product.avif"
 import ryzen7 from '../assets/components/procesor.webp'
 import hdd from '../assets/components/hdd.png'
+import ssd from '../assets/components/KINGSTON_ssd.png'
+import ram from '../assets/components/Kingston-FURY-RENEGADE-16GB.webp'
 const components = [
     {
         name: "be quiet! PURE POWER 12 M 850W",
@@ -19,6 +21,18 @@ const components = [
         price: 736,
         image: hdd,
         link: "https://www.fantastic.md/hdd-seagate-barracuda-compute-st1000dm010/139-217831"
+    },
+    {
+        name: "RAM KINGSTON 16GB DDR4-3600MHz FURY Renegade",
+        price: 1.107,
+        image: ram,
+        link: "https://www.fantastic.md/ram-kingston-16gb-ddr4-3600mhz-fury-renegade-kf436c16rb12-16-cl16-20-20-1-35v-intel-xmp-2-0-black/135-348101"
+    },
+    {
+        name: "SSD KINGSTON KC3000",
+        price: 2264,
+        image: ssd,
+        link: "https://www.fantastic.md/ssd-kingston-kc3000-skc3000s-1024g/765-280264"
     }
 ]
 
