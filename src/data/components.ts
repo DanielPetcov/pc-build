0927bd690799418b71c79b19db22191083921f97
daniel petcov <danielpetcov.work@gmail.com>
2025-01-06 18:13:43 +0200

    updated the components

--- src/data/components.ts
+++ src/data/components.ts
@@ -1,18 +1,14 @@
 import beQuiet from "../assets/components/be-quiet-pure-power-12-m-psu-product.avif"
 import ryzen7 from '../assets/components/procesor.webp'
-import hdd from '../assets/components/hdd.png'
 import ssd from '../assets/components/KINGSTON_ssd.png'
 import ram from '../assets/components/Kingston-FURY-RENEGADE-16GB.webp'
 import deepcool from '../assets/components/deepcool.webp'
 import asrock from '../assets/components/asrock.jpg'
+import ssd_storage from '../assets/components/GOODRAM_CX400.webp'
+import compute_case from '../assets/components/DEEPCOOL_CH560_Digital.webp'
+import gpu from '../assets/components/3070.png'
+
 const components = [
-    {
-        name: "be quiet! PURE POWER 12 M 850W",
-        price: 2722,
-        image: beQuiet,
-        link: "https://electro.md/psu-be-quiet-pure-power-12-m-850w?search=PURE%20POWER%2012%20M",
-        bought: false
-    },
     {
         name: "Ryzen 7 5700X3D",
         price: 3399,
@@ -21,10 +17,45 @@ const components = [
         bought: false,
     },
     {
-        name: "HDD SEAGATE BarraCuda 1TB",
-        price: 736,
-        image: hdd,
-        link: "https://www.fantastic.md/hdd-seagate-barracuda-compute-st1000dm010/139-217831",
+        name: "ASROCK B550 PRO4",
+        price: 2344,
+        image: asrock,
+        link: "https://www.fantastic.md/placa-de-baza-asrock-b550-pro4/134-249165",
+        bought: false,
+    },
+    {
+        name: "be quiet! PURE POWER 12 M 850W",
+        price: 2722,
+        image: beQuiet,
+        link: "https://electro.md/psu-be-quiet-pure-power-12-m-850w?search=PURE%20POWER%2012%20M",
+        bought: false
+    },
+    {
+        name: "DEEPCOOL CH560 Digital",
+        price: 2138,
+        image: compute_case,
+        link: "https://www.fantastic.md/carcasa-fara-psu-deepcool-ch560-digital-w-o-psu-3x140mm-1x120mm-argb-hybrid-tempered-glass-digit-displ/143-334514",
+        bought: false
+    },
+    {
+        name: "DEEPCOOL AS500 PLUS",
+        price: 1435,
+        image: deepcool,
+        link: "https://www.fantastic.md/cooler-universal-deepcool-as500-plus/133-229210",
+        bought: false
+    },
+    {
+        name: "GOODRAM CX400",
+        price: 1275,
+        image: ssd_storage,
+        link: "https://www.fantastic.md/ssd-goodram-cx400-gen-2/928-227285",
+        bought: false
+    },
+    {
+        name: "probabil 3070",
+        price: 5000,
+        image: gpu,
+        link: "#",
         bought: false
     },
     {
@@ -41,20 +72,6 @@ const components = [
         link: "https://www.fantastic.md/ssd-kingston-kc3000-skc3000s-1024g/765-280264",
         bought: true
     },
-    {
-        name: "DEEPCOOL AS500 PLUS",
-        price: 1435,
-        image: deepcool,
-        link: "https://www.fantastic.md/cooler-universal-deepcool-as500-plus/133-229210",
-        bought: false
-    },
-    {
-        name: "ASROCK B550 PRO4",
-        price: 2344,
-        image: asrock,
-        link: "https://www.fantastic.md/placa-de-baza-asrock-b550-pro4/134-249165",
-        bought: false,
-    }
 ]
 
 export default components
--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,10 +1,34 @@
 import ProductCard from "../components/productCard"
 import components from "../data/components"
 
+interface Component { 
+    name: string;
+    price: number;
+    image: string;
+    link: string;
+    bought: boolean
+}
+
 const MainPage = () => {
+
+    const sortComponents = (components : Component[]) => {
+        let boughtComponents = [];
+        let notBoughtComponents = [];
+
+        for(let i = 0; i < components.length; i++){
+            if(components[i].bought){
+                boughtComponents.push(components[i]);
+            } else {
+                notBoughtComponents.push(components[i]);
+            }
+        }
+
+        return notBoughtComponents.concat(boughtComponents);
+    }
+
     return (
         <div className="grid grid-cols-2 lg:grid-cols-3 max-w-[1500px] mx-auto gap-2 gap-y-4 lg:gap-4">
-           {components.map((component) => (
+           {sortComponents(components).map((component) => (
             <ProductCard product={component} />
            ))}
         </div>
